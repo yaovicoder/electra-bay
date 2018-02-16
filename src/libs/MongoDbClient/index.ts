bf6036a83637e705dc47a72a1b237436fd5bd8e6
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-16 21:12:50 +0100

    libs/MongoDbClient/index: add findByIdAndUpdate() method

--- src/libs/MongoDbClient/index.ts
+++ src/libs/MongoDbClient/index.ts
@@ -62,6 +62,29 @@ export default class MongoDbClient {
     })
   }
 
+  public async findByIdAndUpdate<T extends mongoose.Document>(modelName: keyof Models, id: any): Promise<T[]> {
+    if (!clientIsConnected) {
+      const [err] = await to(this.connect())
+      if (err !== null) return Promise.reject(err)
+    }
+
+    // tslint:disable-next-line:variable-name
+    const Model: mongoose.Model<mongoose.Document> = MODELS[modelName]
+
+    return new Promise<T[]>((resolve: (result: T[]) => void, reject: (err: Error) => void): void => {
+      Model.findByIdAndUpdate(id, (err: Error, res: T[]) => {
+        if (err !== null) {
+          log.err(err.message)
+          reject(new Error(`libs/MongoDbClient: Model.findByIdAndUpdate() failed.`))
+
+          return
+        }
+
+        resolve(res)
+      })
+    })
+  }
+
   public async findOne<T extends mongoose.Document>(modelName: keyof Models, conditions: any = {}): Promise<T> {
     if (!clientIsConnected) {
       const [err] = await to(this.connect())
