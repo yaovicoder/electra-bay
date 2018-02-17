a539bf84e567318c6366e15d0498cc2a8a31ae73
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-17 06:27:02 +0100

    controllers/index: tree categories and cache them in Redis

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,21 +2,22 @@ import * as lexpress from 'lexpress'
 import * as R from 'ramda'
 
 import MongoDbClient from '../libs/MongoDbClient'
-import { Category } from '../models/Category'
+import RedisClient from '../libs/RedisClient'
+import { Category, CategoryTreeBranch } from '../models/Category'
 
 const DEFAULT_CACHE_EXPIRATION_IN_SECONDS: number = 1
+const ONE_DAY_IN_SECONDS: number = 86400
 
 export default class BaseController extends lexpress.BaseController {
   protected db: MongoDbClient = new MongoDbClient()
   protected isJson: boolean = false
+  protected redis: RedisClient = new RedisClient()
 
   protected render(view: string, options: any = {}): void {
     const flash: {} = this.req.flash()
 
-    this.db.find<Category>('Category', {
-      parent: null
-    })
-      .then((categories: Category[]) => this.res.render(view, {
+    this.getCategories()
+      .then((categories: CategoryTreeBranch[]) => this.res.render(view, {
         ...options,
         categories,
         flash: R.equals(flash, {}) ? undefined : flash,
@@ -33,10 +34,8 @@ export default class BaseController extends lexpress.BaseController {
   ): void {
     const flash: {} = this.req.flash()
 
-    this.db.find<Category>('Category', {
-      parent: null
-    })
-      .then((categories: Category[]) => this.res.cache(cacheForInSeconds).render(view, {
+    this.getCategories()
+      .then((categories: CategoryTreeBranch[]) => this.res.cache(cacheForInSeconds).render(view, {
         ...options,
         categories,
         flash: R.equals(flash, {}) ? undefined : flash,
@@ -44,4 +43,62 @@ export default class BaseController extends lexpress.BaseController {
       }))
       .catch(this.answerError)
   }
+
+  protected async getCategories(): Promise<CategoryTreeBranch[]> {
+    const categories: CategoryTreeBranch[] | undefined = await this.redis.cache<CategoryTreeBranch[]>('categories')
+
+    if (categories !== undefined) return categories
+
+    return this.cacheCategoriesTree()
+  }
+
+  protected async cacheCategoriesTree(): Promise<CategoryTreeBranch[]> {
+    const categories: CategoryTreeBranch[] = await this.getCategoriesTree()
+    await this.redis.cache('categories', categories, ONE_DAY_IN_SECONDS)
+
+    return categories
+  }
+
+  /**
+   * Transform the MongoDB Category collection into an ordered tree.
+   */
+  protected async getCategoriesTree(
+    categories?: CategoryTreeBranch[],
+    depth: number = 0
+  ): Promise<CategoryTreeBranch[]> {
+    if (categories === undefined) {
+      const categoryDocuments: Category[] = await this.db.find<Category>('Category')
+
+      return categoryDocuments.map((category: Category): CategoryTreeBranch => ({
+        id: category._id.toString(),
+        parent: category.parent === undefined ? undefined : category.parent.toString(),
+        name: category.name,
+        slug: category.slug,
+        position: category.position,
+        depth: category.parent === null ? 0 : undefined,
+        children: [],
+      }))
+    }
+
+    if (categories.filter((category: CategoryTreeBranch) => category.depth === undefined).length === 0) {
+      return this.sortByPosition(categories.filter((category: CategoryTreeBranch) => category.depth === 0))
+    }
+
+    return this.getCategoriesTree(
+      categories.map((category: CategoryTreeBranch) => {
+        if (category.depth !== depth) return category
+
+        category.children = this.sortByPosition(
+          categories
+            .filter((childCategory: CategoryTreeBranch) => childCategory.parent === category.id)
+            .map((childCategory: CategoryTreeBranch) => ({ ...childCategory, ...{ depth: depth + 1 } }))
+        )
+
+        return category
+      }),
+      depth + 1
+    )
+  }
+
+  private sortByPosition: <T>(list: ReadonlyArray<T>) => T[] = R.sortBy(R.prop('position'))
 }
