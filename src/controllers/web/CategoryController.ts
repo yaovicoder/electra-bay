6541d0b0c68270633d7497bcc1ef4409c94159aa
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-17 08:17:06 +0100

    controllers/web/CategoryController: handle recursive chains

--- src/controllers/web/CategoryController.ts
+++ src/controllers/web/CategoryController.ts
@@ -1,3 +1,5 @@
+import * as R from 'ramda'
+
 import BaseController from '..'
 
 import { Category } from '../../models/Category'
@@ -7,25 +9,27 @@ const HTTP_STATUS_CODE_NOT_FOUND: number = 404
 
 export default class CategoryController extends BaseController {
   public get(): void {
-    this.db.findOne<Category>('Category', {
-      slug: this.req.params.categorySlug
-    })
-      .then((category: Category) => {
-        if (category === null) {
+    this.getCategories()
+      .then((categories: Category[]) => {
+        const categoryCurrent: Category = R.find(R.propEq('slug', this.req.params.categorySlug))(categories)
+        if (categoryCurrent === undefined) {
           this.answerError('Not Found', HTTP_STATUS_CODE_NOT_FOUND)
 
           return
         }
 
-        Promise.all<Category[], Item[]>([
-          this.db.find<Category>('Category', { parent: category._id }),
-          this.db.find<Item>('Item', { category: category._id }),
-        ])
-          .then((res: [Category[], Item[]]) => this.render('web/category', {
-            category,
-            items: res[1],
-            // tslint:disable-next-line:no-magic-numbers
-            subCategory: res[2]
+        const categoryCurrentChain: string[] = [categoryCurrent._id]
+        let categoryCurrentChainLink: Category = categoryCurrent
+        while (categoryCurrentChainLink.parent !== undefined) {
+          categoryCurrentChain.push(String(categoryCurrentChainLink.parent))
+          categoryCurrentChainLink = R.find(R.propEq('_id', categoryCurrentChainLink.parent))(categories)
+        }
+
+        this.db.find<Item>('Item', { category: categoryCurrent._id })
+          .then((items: Item[]) => this.render('web/category', {
+            categoryCurrent,
+            categoryCurrentChain,
+            items,
           }))
           .catch(this.answerError)
       })
