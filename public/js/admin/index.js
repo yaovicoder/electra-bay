1e338fdba7a00a8c653deb224a025132dd1dddec
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-18 04:42:45 +0100

    public/js/admin/index: create

---
+++ public/js/admin/index.js
@@ -0,0 +1,20 @@
+const Helpers = {
+  debounce: (func, wait, immediate) => {
+    var timeout
+    return function() {
+      var context = this, args = arguments
+      var later = function() {
+        timeout = null
+        if (!immediate) func.apply(context, args)
+      }
+      var callNow = immediate && !timeout
+      clearTimeout(timeout)
+      timeout = setTimeout(later, wait)
+      if (callNow) func.apply(context, args)
+    }
+  },
+
+  getMeta(name) {
+    return JSON.parse(document.querySelector(`meta[name="${name}"]`).content)
+  }
+}
