1a322dca2ae3f93b95913263217f3d820473b277
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-23 12:20:51 +0100

    models/schemas: fix match regex

--- src/models/schemas.ts
+++ src/models/schemas.ts
@@ -2,7 +2,7 @@ import { SchemaDefinition } from 'mongoose'
 
 export const slug: SchemaDefinition[0] = {
   type: String,
-  match: /^([a-z]{1,2}|[a-z][a-z\-]+[a-z])$/,
+  match: /^([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])$/,
   required: true,
   unique: true,
 }
